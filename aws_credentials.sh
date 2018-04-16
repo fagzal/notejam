8b267defc0f1fe73c7c89306e2cf7c91b65541e3
Fagzal - DevelVPS <csongor.fagyal@kepesmedia.com>
2018-04-16 19:05:28 +0200

    move aws configure to travis

--- aws_credentials.sh
+++ aws_credentials.sh
@@ -1,9 +1,13 @@
 #!/usr/bin/env bash
 
-mkdir -p ~/.aws
+#mkdir -p ~/.aws
+#
+#cat > ~/.aws/credentials << EOL
+#[default]
+#aws_access_key_id = ${AWS_ACCESS_KEY_ID}
+#aws_secret_access_key = ${AWS_SECRET_ACCESS_KEY}
+#EOL
 
-cat > ~/.aws/credentials << EOL
-[default]
-aws_access_key_id = ${AWS_ACCESS_KEY_ID}
-aws_secret_access_key = ${AWS_SECRET_ACCESS_KEY}
-EOL
+aws configure set aws_access_key_id ${AWS_ACCESS_KEY_ID}
+aws configure set ws_secret_access_key = ${AWS_SECRET_ACCESS_KEY}
+aws configure set default.region us-east-2
