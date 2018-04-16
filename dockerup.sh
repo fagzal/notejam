0e18900f7bafcf49d2610a41ce1e61f5fb6fdfdc
Fagzal - DevelVPS <csongor.fagyal@kepesmedia.com>
2018-04-16 23:20:56 +0200

    fix parameter mismatch on docker run call

--- dockerup.sh
+++ dockerup.sh
@@ -4,7 +4,7 @@
 # Load pushed docker image
 docker load < /tmp/notejam.tar
 # Run container as name...
-docker run -d -p --name notejam_container 80:80 notejam:latest
+docker run -d -p 80:80 --name notejam_container notejam:latest
 sleep 3
 # Run migrate, override env to local (since this is a console run via artisan)
 docker exec -t notejam_container bash -c "cd /var/www/html/;php artisan migrate --env=local"
