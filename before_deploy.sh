7a27fd13490691328f957f372d1dc3d36f62e28b
Fagzal - DevelVPS <csongor.fagyal@kepesmedia.com>
2018-04-16 22:49:48 +0200

    overwrite zip bundle without asking

--- before_deploy.sh
+++ before_deploy.sh
@@ -3,7 +3,7 @@
 
 # Get, install, configure AWS
 curl "https://s3.amazonaws.com/aws-cli/awscli-bundle.zip" -o "awscli-bundle.zip"
-unzip awscli-bundle.zip
+unzip -o awscli-bundle.zip
 ./awscli-bundle/install -b ~/bin/aws
 export PATH=~/bin:$PATH
 aws configure set aws_access_key_id ${AWS_ACCESS_KEY_ID}
