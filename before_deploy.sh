213ab7363b4fea0287b1de810668cd336cf42d73
Fagzal - DevelVPS <csongor.fagyal@kepesmedia.com>
2018-04-16 22:24:38 +0200

    refactor travis build and deploy

--- before_deploy.sh
+++ before_deploy.sh
@@ -1,28 +1,21 @@
-#!/usr/bin/env bash
+#!/bin/bash
+# This script is run before an AWS deployment is done.
 
-#mkdir -p ~/.aws
-#
-#cat > ~/.aws/credentials << EOL
-#[default]
-#aws_access_key_id = ${AWS_ACCESS_KEY_ID}
-#aws_secret_access_key = ${AWS_SECRET_ACCESS_KEY}
-#EOL
-
-
+# Get, install, configure AWS
 curl "https://s3.amazonaws.com/aws-cli/awscli-bundle.zip" -o "awscli-bundle.zip"
 unzip awscli-bundle.zip
 ./awscli-bundle/install -b ~/bin/aws
 export PATH=~/bin:$PATH
 aws configure set aws_access_key_id ${AWS_ACCESS_KEY_ID}
 aws configure set ws_secret_access_key ${AWS_SECRET_ACCESS_KEY}
-aws configure set default.region us-east-2
+aws configure set default.region ${AWS_DEFAULT_ZONE}
 RDSHOST=$(aws rds describe-db-instances --db-instance-identifier aurorac --query 'DBInstances[0].Endpoint.Address')
 cat > laravel/notejam/app/config/local/database.php << EOL
 <?php
 
 return array(
         'driver'    => 'mysql',
-        'host'      => '${RDSHOST}',
+        'host'      => ${RDSHOST},
         'database'  => 'notejam',
         'username'  => 'notejamuser',
         'password'  => 'xxxx',
@@ -31,6 +24,12 @@ return array(
         'prefix'    => '',
 );
 EOL
+
+# rebuild image as files has changed
+docker build -f docker/Dockerfile_apache -t notejam:latest .
+mkdir build
+docker save notejam:latest > build/notejam.tar
+zip -r build/bundle.zip build/notejam.tar appspec.yml dockerup.sh 
 
 #echo ${RDSHOST} > laravel/notejam/app/config/local/database.php
 
--- dockerup.sh
+++ dockerup.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 docker load < /tmp/notejam.tar
-docker run -d -p 80:80 notejam:latest
+docker run -d -p --name notejam_container 80:80 notejam:latest
+sleep 3
+docker exec -t notejam_container bash -c "cd /var/www/html/;php artisan migrate --env=local"
